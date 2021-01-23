6473ea38182c06c1df48838c979aa3e2fa2b10df
tttooommmooo <tabinn.0803.koyumayu@gmail.com>
2021-01-23 15:12:07 +0900

    カラム名の修正

---
+++ db/migrate/20210123054411_rename_prototypes_id_column_to_comments.rb
@@ -0,0 +1,4 @@
+class RenamePrototypesIdColumnToComments < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_100350) do
+ActiveRecord::Schema.define(version: 2021_01_23_054411) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -36,10 +36,10 @@ ActiveRecord::Schema.define(version: 2021_01_19_100350) do
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text", null: false
     t.bigint "user_id"
-    t.bigint "prototypes_id"
+    t.bigint "prototype_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["prototypes_id"], name: "index_comments_on_prototypes_id"
+    t.index ["prototype_id"], name: "index_comments_on_prototype_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 2021_01_19_100350) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "comments", "prototypes", column: "prototypes_id"
+  add_foreign_key "comments", "prototypes"
   add_foreign_key "comments", "users"
   add_foreign_key "prototypes", "users"
 end
